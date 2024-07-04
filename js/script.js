83773ccae527a6bfb970e4a85d66c8130e793296
Matthis Böhm <matthis.boehm2@web.de>
2024-07-04 19:35:26 +0200

    Add delete and read status button to cards

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@ const myLibrary = [];
 // DOM elements
 const container = document.querySelector(".container");
 const dialog = document.querySelector("dialog");
-const newBookBtn = document.querySelector("dialog + button");
+const newBookBtn = document.querySelector(".new-book");
 const closeBtn = document.querySelector("dialog #closeBtn");
 const confirmBtn = document.querySelector("dialog #confirmBtn");
 
 const inputTitle = document.querySelector("#title");
 const inputAuthor = document.querySelector("#author");
 const inputPages = document.querySelector("#pages");
-const inputRead = document.querySelector("#read");
+const btnRead = document.querySelector("#read");
 
 // Constructors
 function Book(title, author, pages, read) {
@@ -20,12 +20,14 @@ function Book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
   this.displayed = false;
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-  };
 }
 
-// Methods
+// Prototype functions
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
+
+// Functions
 function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
@@ -47,22 +49,42 @@ function displayBooks() {
     divTitle.textContent = book.title;
     divAuthor.textContent = book.author;
     divPages.textContent = book.pages;
-    divRead.textContent = book.read;
+    divRead.textContent = `Already read: ${book.read}`;
+
+    const btnDelete = document.createElement("button");
+    btnDelete.textContent = "Delete";
+    btnDelete.addEventListener("click", () => {
+      container.removeChild(divCard);
+      myLibrary.splice(myLibrary.indexOf(book), 1);
+    });
+
+    const btnToggleRead = document.createElement("button");
+    btnToggleRead.textContent = "Change read status";
+    btnToggleRead.addEventListener("click", () => {
+      book.toggleRead();
+      divRead.textContent = `Already read: ${book.read}`;
+    });
 
     divCard.appendChild(divTitle);
     divCard.appendChild(divAuthor);
     divCard.appendChild(divPages);
     divCard.appendChild(divRead);
 
+    divCard.appendChild(btnDelete);
+    divCard.appendChild(btnToggleRead);
+
     container.appendChild(divCard);
   });
 }
 
-function resetInputFields() {
+function resetFormFields() {
   inputTitle.value = "";
   inputAuthor.value = "";
   inputPages.value = "";
-  inputRead.value = "";
+  btnRead.textContent = "Unread";
+  if (btnRead.classList.contains("read")) {
+    btnRead.classList.remove("read");
+  }
 }
 
 // Event listeners
@@ -73,27 +95,31 @@ confirmBtn.addEventListener("click", () => {
     inputTitle.value,
     inputAuthor.value,
     inputPages.value,
-    inputRead.value
+    btnRead.textContent === "Read" ? true : false
   );
   displayBooks();
-  resetInputFields();
+  resetFormFields();
   dialog.close();
+});
+btnRead.addEventListener("click", () => {
+  btnRead.classList.toggle("read");
+  btnRead.textContent = btnRead.classList.contains("read") ? "Read" : "Unread";
 });
 
 //Programm start
-// const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
-// const book2 = new Book("Harry Potter", "J.K. Rowling", 471, "not read yet");
-// const book3 = new Book("Harry Potter", "J.K. Rowling", 471, "not read yet");
-// const book4 = new Book("Harry Potter", "J.K. Rowling", 471, "not read yet");
-// const book5 = new Book("Harry Potter", "J.K. Rowling", 471, "not read yet");
-// const book6 = new Book("Harry Potter", "J.K. Rowling", 471, "not read yet");
-// const book7 = new Book("Harry Potter", "J.K. Rowling", 471, "not read yet");
-// myLibrary.push(book1);
-// myLibrary.push(book2);
-// myLibrary.push(book3);
-// myLibrary.push(book4);
-// myLibrary.push(book5);
-// myLibrary.push(book6);
-// myLibrary.push(book7);
+const book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const book2 = new Book("Harry Potter", "J.K. Rowling", 471, false);
+const book3 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const book4 = new Book("Harry Potter", "J.K. Rowling", 471, false);
+const book5 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+const book6 = new Book("Harry Potter", "J.K. Rowling", 471, false);
+const book7 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
+myLibrary.push(book4);
+myLibrary.push(book5);
+myLibrary.push(book6);
+myLibrary.push(book7);
 
 displayBooks();
